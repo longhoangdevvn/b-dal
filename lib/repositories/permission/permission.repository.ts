28e1f4bc8afce4999d7d51ed97c4b8e9a8f4786b
Long Hoang <longhoangdevvn@gmail.com>
2024-05-09 21:15:55 +0700

    update: permission repository

--- lib/repositories/permission/permission.repository.ts
+++ lib/repositories/permission/permission.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { BaseRepository } from '../base.repository';
 import { PermissionEntity } from '../../entities';
-import { Repository } from 'typeorm';
+import { In, Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
@@ -11,5 +11,73 @@ export class PermissionRepository extends BaseRepository<PermissionEntity> {
     private readonly PermissionRepository: Repository<PermissionEntity>,
   ) {
     super(PermissionRepository);
+  }
+
+  // List all permissions
+  permissions(): Promise<PermissionEntity[]> {
+    return this.PermissionRepository.find({
+      where: {
+        deletedAt: null,
+      },
+    });
+  }
+
+  // Get a permission by id
+  permission(id: number): Promise<PermissionEntity> {
+    if (!id) {
+      return null;
+    }
+    return this.PermissionRepository.findOne({
+      where: {
+        id,
+        deletedAt: null,
+      },
+    });
+  }
+
+  // Create a new permission
+  createPermission(permission): Promise<PermissionEntity> {
+    return this.PermissionRepository.save(permission);
+  }
+
+  // Create multiple permissions
+  createPermissions(permissions): Promise<PermissionEntity[]> {
+    return this.PermissionRepository.save(permissions);
+  }
+
+  // Update a permission
+  updatePermission(id: number, permission): Promise<UpdateResult> {
+    if (!id) {
+      return null;
+    }
+    return this.PermissionRepository.update(id, permission);
+  }
+
+  // Destroy a permission
+  destroyPermission(id: number): Promise<UpdateResult> {
+    if (!id) {
+      return null;
+    }
+    return this.PermissionRepository.softDelete(id);
+  }
+
+  // Get permissions by code
+  permissionsByCode(codes: string[]): Promise<PermissionEntity[]> {
+    return this.PermissionRepository.find({
+      where: {
+        code: In(codes),
+        deletedAt: null,
+      },
+    });
+  }
+
+  // Get permissions by group id
+  permissionsByGroupId(groupId: number): Promise<PermissionEntity[]> {
+    return this.PermissionRepository.find({
+      where: {
+        groupId,
+        deletedAt: null,
+      },
+    });
   }
 }
